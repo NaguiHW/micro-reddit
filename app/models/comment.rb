4f85c11c9cf0a13b5c650f364686f74fbac54fa3
Kalib Hackin <nagui08@hotmail.com>
2019-06-27 16:57:24 -0500

    fixing some stickler issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Comment < ApplicationRecord
-    validates :body, :user_id, :post_id, presence: true
-    belongs_to :user
-    belongs_to :post
+  validates :body, :user_id, :post_id, presence: true
+  belongs_to :user
+  belongs_to :post
 end
--- db/migrate/20190627214353_change_data_type_for_body.rb
+++ db/migrate/20190627214353_change_data_type_for_body.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeDataTypeForBody < ActiveRecord::Migration[5.2]
   def change
     change_column :comments, :body, :text
